6cc1dba2e8abde5ac9fdd249612da23a3fd7bfbc
Tom Westerhout <t.westerhout@student.ru.nl>
2019-08-22 00:15:46 +0200

    Make sure particle.hpp compiles

---
+++ test/headers/neighbour_stats.cpp
@@ -0,0 +1,6 @@
+#include "detail/neighbour_stats.hpp"
+
+using cluster_type = double;
+template struct tcm::neighbour_stats_t<cluster_type, 8>;
+
+auto main() -> int {}
--- test/headers/particle.cpp
+++ test/headers/particle.cpp
@@ -1,3 +1,34 @@
 #include "detail/particle.hpp"
 
-int main() {}
+struct dummy_system_t {
+    template <class T> struct unique_ptr {
+        void* data[2];
+
+        unique_ptr()                      = delete;
+        unique_ptr(unique_ptr const&)     = delete;
+        unique_ptr(unique_ptr&&) noexcept = default;
+        auto operator=(unique_ptr const&) -> unique_ptr& = delete;
+        auto operator=(unique_ptr&&) noexcept -> unique_ptr& = default;
+
+        friend auto operator==(unique_ptr const& x, std::nullptr_t) noexcept
+            -> bool
+        {
+            return x.data[0] == nullptr;
+        }
+
+        friend auto operator!=(unique_ptr const& x, std::nullptr_t) noexcept
+            -> bool
+        {
+            return !(x == nullptr);
+        }
+
+        auto operator*() const noexcept -> T&
+        {
+            return *reinterpret_cast<T*>(data[1]);
+        }
+    };
+};
+
+template union tcm::particle_t<dummy_system_t>;
+
+auto main() -> int {}
