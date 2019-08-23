85c63153a43636c0e6eae6594a60a4a729d9c024
Tom Westerhout <t.westerhout@student.ru.nl>
2019-08-23 02:32:05 +0200

    Try compiling implementation files as well

---
+++ test/headers/geometric_cluster.cpp
@@ -0,0 +1,6 @@
+#include "detail/geometric_cluster.hpp"
+#if defined(TCM_TEST_FULL)
+#    include "detail/geometric_cluster.ipp"
+#endif
+
+auto main() -> int {}
--- test/headers/magnetic_cluster.cpp
+++ test/headers/magnetic_cluster.cpp
@@ -1,3 +1,6 @@
 #include "detail/magnetic_cluster.hpp"
+#if defined(TCM_TEST_FULL)
+#    include "detail/magnetic_cluster.ipp"
+#endif
 
 auto main() -> int {}
